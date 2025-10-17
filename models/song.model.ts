ef5a0c913dc1f80de6db047e00fb275dd9fa17b6
quan <quan93909@gmail.com>
2025-10-17 21:45:02 +0700

    Làm giao diện tạo bài hát ( admin )

--- models/song.model.ts
+++ models/song.model.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import slug from "mongoose-slug-updater";
+
+mongoose.plugin(slug);
 
 const songSchema = new mongoose.Schema(
   {
@@ -10,7 +13,11 @@ const songSchema = new mongoose.Schema(
     lyrics: String,
     audio: String,
     status: String,
-    slug: String,
+    slug: {
+      type: String,
+      slug: "title",
+      unique: true
+    },
     like: {
       type: Number,
       default: 0
