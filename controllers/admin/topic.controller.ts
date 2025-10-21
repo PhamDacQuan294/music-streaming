e79ff81dd1706403863abec548810891b775cc95
binh <binlun411@gmail.com>
2025-10-21 21:41:20 +0700

    Tính năng lọc theo trạng thái (Topic)

--- controllers/admin/topic.controller.ts
+++ controllers/admin/topic.controller.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
+import { filterStatus } from "../../helpers/filterStatus";
 
 // [GET] /admin/topics
 export const index = async (req: Request, res: Response) => {
-  const topics = await Topic.find({
+  const statusFilters = filterStatus(req.query);
+
+  let find = {
     deleted: false
-  });
+  }
+
+  if (req.query.status) {
+    find["status"] = req.query.status;
+  }
+
+  const topics = await Topic.find(find);
 
   res.render("admin/pages/topics/index", {
     pageTitle: "Quản lý chủ đề",
-    topics: topics
+    topics: topics,
+    filterStatus: statusFilters,
   });
 }
