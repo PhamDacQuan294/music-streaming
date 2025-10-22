4a961347e8949faf4e6fbf47fee55b59885981db
quang <quangnd170105@gmail.com>
2025-10-22 17:22:58 +0700

    Tính năng tìm kiếm chủ đề admin

--- controllers/admin/topic.controller.ts
+++ controllers/admin/topic.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
 import { filterStatus } from "../../helpers/filterStatus";
+import { convertToSlug } from "../../helpers/convertToSlug";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
 
 // [GET] /admin/topics
 export const index = async (req: Request, res: Response) => {
@@ -14,11 +17,29 @@ export const index = async (req: Request, res: Response) => {
     find["status"] = req.query.status;
   }
 
+   let keyword: string = "";
+
+  if (req.query.keyword) {
+    keyword = `${req.query.keyword}`;
+    
+    const keywordRegex = new RegExp(keyword, "i");
+
+    // Tạo ra slug không dấu, có thêm dấu - ngăn cách
+    const stringSlug = convertToSlug(keyword);
+    const stringSlugRegex = new RegExp(stringSlug, "i");
+
+    find["$or"] = [
+      { title: keywordRegex },
+      { slug: stringSlugRegex }
+    ];
+  }
+
   const topics = await Topic.find(find);
 
   res.render("admin/pages/topics/index", {
     pageTitle: "Quản lý chủ đề",
     topics: topics,
     filterStatus: statusFilters,
+    keyword: keyword
   });
 }
