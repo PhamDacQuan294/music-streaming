acdad96b3c9fed9989ff7202b7e464f79fd8fb2d
PC thai <pthai345789@gmail.com>
2025-11-09 20:26:34 +0700

    Làm trang sửa tài khoản admin

--- controllers/admin/account.controller.ts
+++ controllers/admin/account.controller.ts
@@ -58,3 +58,57 @@ export const createPost = async (req: Request, res: Response) => {
     res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
   }
 }
+
+
+// [GET] /admin/accounts/edit/:id
+export const edit = async (req: Request, res: Response) => {
+  let find = {
+    _id: req.params.id,
+    deleted: false
+  };
+
+  try {
+    const data = await Account.findOne(find).select("-password");
+
+    const roles = await Role.find({
+      deleted: false,
+    });
+
+    res.render("admin/pages/accounts/edit", {
+      pageTitle: "Chỉnh sửa tài khoản",
+      data: data,
+      roles: roles,
+    });
+
+  } catch (error) {
+    res.redirect(`/${systemConfig.prefixAdmin}/accounts`);
+  }
+}
+
+// [PATCH] /admin/accounts/edit/:id
+export const editPatch = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const emailExist = await Account.findOne({
+    _id: { $ne: id },
+    email: req.body.email,
+    deleted: false
+  });
+
+  if(emailExist) {
+    (req as any).flash("error", `Email ${req.body.email} đã tồn tại`);
+  } else {
+
+    if(req.body.password) {
+      req.body.password = md5(req.body.password);
+    } else {
+      delete req.body.password;
+    }
+
+    (req as any).flash("success", "Cập nhật tài khoản thành công!");
+
+    await Account.updateOne({ _id: id }, req.body);
+  }
+
+  res.redirect(`/${systemConfig.prefixAdmin}/accounts/edit/${id}`);
+}
--- routes/admin/account.route.ts
+++ routes/admin/account.route.ts
@@ -19,4 +19,13 @@ router.post(
   controller.createPost
 );
 
+router.get("/edit/:id", controller.edit);
+
+router.patch(
+  "/edit/:id", 
+  upload.single("avatar"),
+  uploadCloud.uploadSingle,
+  controller.editPatch
+);
+
 export const accountRoutes: Router = router;
