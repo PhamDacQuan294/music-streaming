d3768af025f297cc490c21a185be37fb97abd12e
quan <quan93909@gmail.com>
2025-11-10 15:12:43 +0700

    Thêm middleware cho route private admin

--- controllers/admin/auth.controller.ts
+++ controllers/admin/auth.controller.ts
@@ -6,40 +6,50 @@ import md5 from "md5";
 
 // [GET] /admin/auth/login
 export const login = async (req: Request, res: Response) => {
-  res.render("admin/pages/auth/login", {
-    pageTitle: "Đăng nhập",
-  });
+  if(req.cookies.token) {
+    res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
+  } else {
+    res.render("admin/pages/auth/login", {
+      pageTitle: "Đăng nhập",
+    });
+  }
 };
 
 // [POST] /admin/auth/login
 export const loginPost = async (req: Request, res: Response) => {
-  const email = req.body.email;
-  const password = req.body.password;
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    const user = await Account.findOne({
+      email: email,
+      deleted: false,
+    });
 
-  const user = await Account.findOne({
-    email: email,
-    deleted: false,
-  });
+    if (!user) {
+      (req as any).flash("error", "Email không tồn tại!");
+      (req as any).flash("old", req.body);
+      res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
+      return;
+    }
 
-  if (!user) {
-    (req as any).flash("error", "Email không tồn tại!");
-    (req as any).flash("old", req.body);
-    res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
-    return;
+    if (md5(password) != user.password) {
+      (req as any).flash("error", "Sai mật khẩu!");
+      (req as any).flash("old", req.body);
+      res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
+      return;
+    }
+
+    if (user.status == "inactive") {
+      (req as any).flash("error", "Taì khoản đã bị xoá!");
+      res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
+      return;
+    }
+    res.cookie("token", user.token);
+
+    res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);  
+      
+  } catch (error) {
+    console.log(error);
   }
-
-  if (md5(password) != user.password) {
-    (req as any).flash("error", "Sai mật khẩu!");
-    (req as any).flash("old", req.body);
-    res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
-    return;
-  }
-
-  if (user.status == "inactive") {
-    (req as any).flash("error", "Taì khoản đã bị xoá!");
-    res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
-    return;
-  }
-
-  res.redirect(`/${systemConfig.prefixAdmin}/dashboard`);
 };
---
+++ middlewares/admin/auth.middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import { systemConfig } from "../../config/config";
+import Account from "../../models/account.model";
+
+export const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
+  if(!req.cookies.token) {
+    res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
+    console.log(req.cookies);
+  } else {
+    const user = await Account.findOne({ token: req.cookies.token });
+    if(!user) {
+      res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
+    } else {
+      next();
+    }
+  }
+}
